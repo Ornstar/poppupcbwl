9b69ce2127aae02dd608a10e68895b3b1ab6ac98
bot <manaakumana51@gmail.com>
2026-04-15 09:30:20 +0700

    Update popup banner image for desktop version

--- popupcb.js
+++ popupcb.js
@@ -371,7 +371,7 @@
   <button class="tombol-close" type="button" aria-label="Tutup popup">×</button>
 
   <div class="popup-banner">
-    <img src="https://res.cloudinary.com/dazguhisl/image/upload/v1776219766/CB016_Mobile_y4j63g.png" alt="Banner Popup">
+    <img src="https://res.cloudinary.com/dazguhisl/image/upload/v1776220190/CB016_Desktop_esxbug.png" alt="Banner Popup">
   </div>
 
   <span class="teks-putih-tebal">TEMUKAN LINK RESMI KAMI</span>

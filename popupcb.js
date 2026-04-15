c012886acf8005d6f0861b2961c149ab0a576d22
bot <manaakumana51@gmail.com>
2026-04-15 14:58:09 +0700

    Update popup banner image source URL

--- popupcb.js
+++ popupcb.js
@@ -371,7 +371,7 @@
   <button class="tombol-close" type="button" aria-label="Tutup popup">×</button>
 
   <div class="popup-banner">
-    <img src="https://res.cloudinary.com/dazguhisl/image/upload/v1776220488/CB016_Mobile_2_qfrcpa.png" alt="Banner Popup">
+    <img src="http://plcl.me/images/ddmT4.jpg" alt="Banner Popup">
   </div>
 
   <span class="teks-putih-tebal">TEMUKAN LINK RESMI KAMI</span>
